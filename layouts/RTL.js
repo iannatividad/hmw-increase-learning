1354e9c69ab930f02e90351d582aa2655f79c868
Ian Natividad <ian@symph.co>
2021-05-17 18:41:06 +0800

    [Misc][Ian] update

--- layouts/RTL.js
+++ layouts/RTL.js
@@ -9,7 +9,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Navbar from "components/Navbars/Navbar.js";
 import Footer from "components/Footer/Footer.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
@@ -21,99 +20,90 @@ import logo from "assets/img/reactlogo.png";
 let ps;
 
 export default function RTL({ children, ...rest }) {
-  // used for checking current route
-  const router = useRouter();
-  // styles
-  const useStyles = makeStyles(styles);
-  const classes = useStyles();
-  // ref to help us initialize PerfectScrollbar on windows devices
-  const mainPanel = React.createRef();
-  // states and functions
-  const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("white");
-  const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const handleImageClick = (image) => {
-    setImage(image);
-  };
-  const handleColorClick = (color) => {
-    setColor(color);
-  };
-  const handleFixedClick = () => {
-    if (fixedClasses === "dropdown") {
-      setFixedClasses("dropdown show");
-    } else {
-      setFixedClasses("dropdown");
-    }
-  };
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-  const getRoute = () => {
-    return router.pathname !== "/admin/maps";
-  };
-  const resizeFunction = () => {
-    if (window.innerWidth >= 960) {
-      setMobileOpen(false);
-    }
-  };
-  // initialize and destroy the PerfectScrollbar plugin
-  React.useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps = new PerfectScrollbar(mainPanel.current, {
-        suppressScrollX: true,
-        suppressScrollY: false,
-      });
-      document.body.style.overflow = "hidden";
-    }
-    window.addEventListener("resize", resizeFunction);
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
-        ps.destroy();
-      }
-      window.removeEventListener("resize", resizeFunction);
+    // used for checking current route
+    const router = useRouter();
+    // styles
+    const useStyles = makeStyles(styles);
+    const classes = useStyles();
+    // ref to help us initialize PerfectScrollbar on windows devices
+    const mainPanel = React.createRef();
+    // states and functions
+    const [image, setImage] = React.useState(bgImage);
+    const [color, setColor] = React.useState("white");
+    const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+    const handleImageClick = (image) => {
+        setImage(image);
     };
-  }, [mainPanel]);
-  return (
-    <div className={classes.wrapper}>
-      <Sidebar
-        routes={routes}
-        logoText={"الإبداعية تيم"}
-        logo={logo}
-        image={image}
-        handleDrawerToggle={handleDrawerToggle}
-        open={mobileOpen}
-        color={color}
-        rtlActive
-        {...rest}
-      />
-      <div className={classes.mainPanel} ref={mainPanel}>
-        <Navbar
-          routes={routes}
-          handleDrawerToggle={handleDrawerToggle}
-          rtlActive
-          {...rest}
-        />
-        {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
-        {getRoute() ? (
-          <div className={classes.content}>
-            <div className={classes.container}>{children}</div>
-          </div>
-        ) : (
-          <div className={classes.map}>{children}</div>
-        )}
-        {getRoute() ? <Footer /> : null}
-        <FixedPlugin
-          handleImageClick={handleImageClick}
-          handleColorClick={handleColorClick}
-          bgColor={color}
-          bgImage={image}
-          handleFixedClick={handleFixedClick}
-          fixedClasses={fixedClasses}
-          rtlActive
-        />
-      </div>
-    </div>
-  );
+    const handleColorClick = (color) => {
+        setColor(color);
+    };
+    const handleFixedClick = () => {
+        if (fixedClasses === "dropdown") {
+            setFixedClasses("dropdown show");
+        } else {
+            setFixedClasses("dropdown");
+        }
+    };
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+    const getRoute = () => {
+        return router.pathname !== "/admin/maps";
+    };
+    const resizeFunction = () => {
+        if (window.innerWidth >= 960) {
+            setMobileOpen(false);
+        }
+    };
+    // initialize and destroy the PerfectScrollbar plugin
+    React.useEffect(() => {
+        if (navigator.platform.indexOf("Win") > -1) {
+            ps = new PerfectScrollbar(mainPanel.current, {
+                suppressScrollX: true,
+                suppressScrollY: false,
+            });
+            document.body.style.overflow = "hidden";
+        }
+        window.addEventListener("resize", resizeFunction);
+        // Specify how to clean up after this effect:
+        return function cleanup() {
+            if (navigator.platform.indexOf("Win") > -1) {
+                ps.destroy();
+            }
+            window.removeEventListener("resize", resizeFunction);
+        };
+    }, [mainPanel]);
+    return (
+        <div className={classes.wrapper}>
+            <Sidebar
+                routes={routes}
+                logoText={"الإبداعية تيم"}
+                logo={logo}
+                image={image}
+                handleDrawerToggle={handleDrawerToggle}
+                open={mobileOpen}
+                color={color}
+                rtlActive
+                {...rest}
+            />
+            <div className={classes.mainPanel} ref={mainPanel}>
+                <Navbar
+                    routes={routes}
+                    handleDrawerToggle={handleDrawerToggle}
+                    rtlActive
+                    {...rest}
+                />
+                {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
+                {getRoute() ? (
+                    <div className={classes.content}>
+                        <div className={classes.container}>{children}</div>
+                    </div>
+                ) : (
+                    <div className={classes.map}>{children}</div>
+                )}
+                {getRoute() ? <Footer /> : null}
+            </div>
+        </div>
+    );
 }
